09357d1f8a274b1ff06a2f2af8f0aca9d495203a
Michael J. Curry <currymj@fastmail.fm>
2016-12-04 14:29:19 -0500

    day 3 complete!

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,17 @@ module Main where
 import Lib1
 import Lib2
 import Data.List.Split
-
+import Data.List (transpose)
+import Lib3
 
 advent2 = readFile "src/inputFile2.txt" >>=
   (putStrLn . concat . map numberFromState . processManyLines . init . splitOn "\n")
 
+splitStringIntoTuple :: String -> [Int]
+splitStringIntoTuple s = map read $ words s
+
+advent3 = readFile "src/inputFile3.txt" >>=
+  --(putStrLn . show . countValidTriangles . map splitStringIntoTuple . init . splitOn "\n")
+  (putStrLn . show . countValidColumnTriangles . transpose . map splitStringIntoTuple . init . splitOn "\n")
 main :: IO ()
-main = advent2
+main = advent3
---
+++ src/Lib3.hs
@@ -0,0 +1,21 @@
+module Lib3 (
+  countValidTriangles, countValidColumnTriangles) where
+import Data.Maybe
+import Data.List.Split
+
+countValidTriangles :: [[Int]] -> Int
+countValidTriangles inputTuples = length . filter isJust $ map triangleList inputTuples
+
+countValidColumnTriangles = length . filter isJust . generateValidColumnTriangles
+generateValidColumnTriangles [al, bl, cl] = map triangleList $ concat $ map (chunksOf 3) [al, bl, cl]
+
+ 
+
+data Triangle = Triangle Int Int Int
+triangle a b c
+  | a + b > c && b + c > a && a + c > b = Just (Triangle a b c)
+  | otherwise = Nothing
+
+
+triangleList [a, b, c] = triangle a b c
+
