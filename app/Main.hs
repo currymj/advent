462e84bcf4a3e4dc578c8224fbdeb722195b379f
Michael J. Curry <currymj@fastmail.fm>
2016-12-05 09:33:57 -0500

    part 4 complete!

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ import Lib2
 import Data.List.Split
 import Data.List (transpose)
 import Lib3
+import Lib4
+import Text.Printf
 
 advent2 = readFile "src/inputFile2.txt" >>=
   (putStrLn . concat . map numberFromState . processManyLines . init . splitOn "\n")
@@ -15,5 +17,9 @@ splitStringIntoTuple s = map read $ words s
 advent3 = readFile "src/inputFile3.txt" >>=
   --(putStrLn . show . countValidTriangles . map splitStringIntoTuple . init . splitOn "\n")
   (putStrLn . show . countValidColumnTriangles . transpose . map splitStringIntoTuple . init . splitOn "\n")
+advent4 = readFile "src/rooms.txt" >>=
+  --(putStrLn . show . countComputedIds . init . splitOn "\n")
+  (putStrLn . concat . map (uncurry (printf "%s %d\n")) . getRotValid . init . splitOn "\n")
+
 main :: IO ()
-main = advent3
+main = advent4
--- src/Lib4.hs
+++ src/Lib4.hs
@@ -1,10 +1,14 @@
-module Lib4 where
+module Lib4 (countComputedIds, getRotValid) where
 
 import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Token
 import Control.Monad
+import Data.Maybe
 import Data.Tuple
+import Data.List
+import Data.Ord
+import Data.Either
 
 lettersDash :: Parser [Char]
 lettersDash = liftM concat $ sepEndBy (many1 letter) (char '-')
@@ -14,5 +18,26 @@ sectorId = integer
 checkSum :: Parser [Char]
 checkSum = between (char '[') (char ']') (many1 letter)
 
+
+myTupleCompare (a1, b1) (a2, b2)
+  | a1 < a2 = GT
+  | a1 > a2 = LT
+  | a1 == a2 = compare b1 b2
 allParse :: Parser ([Char], Int, [Char])
 allParse = (,,) <$> lettersDash <*> liftM read (many1 digit) <*> checkSum
+
+computeCheckSum chList = concat . take 5 . map snd $ sortBy myTupleCompare $ map (\x -> (length x, take 1 x)) $ group $ sort chList
+processParsed (Right (ch, num, check)) = if computeCheckSum ch == check then Just num else Nothing
+processParsed (Left _) = Nothing
+
+countComputedIds = sum . map (maybe 0 id) . map (\x -> processParsed $ parse allParse x x)
+
+
+rotChar :: Int -> Char -> Char
+rotChar n ch = toEnum $ (97+) $ mod ((fromEnum ch) - 97 + n) 26
+rot n = map (rotChar n)
+
+processRot (Right (ch, num, check)) = if computeCheckSum ch == check then Just (rot num ch, num) else Nothing
+processRot (Left _) = Nothing
+
+getRotValid = map (maybe ("", 0) id) . filter isJust . map (processRot . parse allParse "")
